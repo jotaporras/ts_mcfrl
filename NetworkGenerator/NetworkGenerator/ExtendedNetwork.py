18726eddd6fe9f49bc92d1fda6acd3ff712ca602
Barnum Castillo Barquero <barnum2011@live.com>
2020-05-19 22:32:10 -0600

    Add ExtendedNetwork

---
+++ NetworkGenerator/NetworkGenerator/ExtendedNetwork.py
@@ -0,0 +1,15 @@
+
+class ExtendedNetwork:
+    """
+    Definition of a network
+    
+    :param Network network:
+    :param Orders orders: 
+    
+    """
+    def __init__(self, network, orders):
+        self.network = network
+        self.orders = orders
+
+
+
--- NetworkGenerator/NetworkGenerator/network/Arc.py
+++ NetworkGenerator/NetworkGenerator/network/Arc.py
@@ -6,7 +6,9 @@ class Arc:
     :param Node nodeFrom: node from where the arc starts
     :param Node nodeTo: node from where the arc ends
     """
-    def __init__(self, id, nodeFrom, nodeTo):
+    def __init__(self, id, nodeFrom, nodeTo, cost, capacity):
         self.id = id
         self.nodeFrom = nodeFrom
         self.nodeTo = nodeTo
+        self.cost = cost
+        self.cost = capacity
